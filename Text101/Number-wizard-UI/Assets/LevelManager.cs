be94fc65c2ee2f9b4cb8ee3ca461aaf6075780bf
Prashanth <prashanthds@live.com>
2017-01-25 14:30:21 +0530

    what ever I want

---
+++ Text101/Number-wizard-UI/Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("Level load requested for: " + name);
+		//Application.loadedLevel (name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("I want to quit");
+		SceneManager.LoadScene("Lose");
+	}
+}
